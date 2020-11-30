76b2fdfbf9e076d8f141d60d6f97048d8b7b1c89
Maxim Remizov <wozimer@gmail.com>
2020-11-30 19:43:00 +0300

    Перенёс чтение фалов внутрь тестов.

--- _unit_tests/image/test_metrics.py
+++ _unit_tests/image/test_metrics.py
@@ -1,41 +1,45 @@
 import unittest
 
-from image import read, write
+from helpers import TestFileHelper
+from image import read
 
 
-class MSEMetric(unittest.TestCase):
-
-    img1 = read('./test_data/test_image_0.jpg')
-    img2 = img1
-    img3 = read('./test_data/test_image_1.jpg')
-    img4 = read('./test_data/test_image_2.jpg')  # shrunk
-    img5 = read('./test_data/test_image_3.jpg')  # rotated
-    img6 = read('./test_data/test_smooth.png')   # smooth
-    img7 = read('./test_data/test_smoothed.png')  # smoothed
+class MSEMetric(unittest.TestCase, TestFileHelper):
 
     def test_identical_images(self):
         # Compare identical images with the same shape.
-        self.assertEqual(self.img1.metrics.mse(self.img2), 0)
+        img1 = read(self._test_file('test_image_0.jpg'))
+        img2 = read(self._test_file('test_image_0.jpg'))
+        self.assertEqual(img1.metrics.mse(img2), 0)
 
     def test_different_images(self):
         # Compare different images with the same shape.
-        self.assertGreater(self.img1.metrics.mse(self.img3), 0)
+        img1 = read(self._test_file('test_image_0.jpg'))
+        img3 = read(self._test_file('test_image_1.jpg'))
+        self.assertGreater(img1.metrics.mse(img3), 0)
 
     def test_shrunk_shape(self):
         # Compare images with different shapes / shrunk.
+        img1 = read(self._test_file('test_image_0.jpg'))
+        img4 = read(self._test_file('test_image_2.jpg'))  # shrunk
 
         with self.assertRaises(ValueError):
-            self.img1.metrics.mse(self.img4)
+            img1.metrics.mse(img4)
 
     def test_rotated_shape(self):
         # Compare images with different shapes / rotated.
+        img1 = read(self._test_file('test_image_0.jpg'))
+        img5 = read(self._test_file('test_image_3.jpg'))  # rotated
 
         with self.assertRaises(ValueError):
-            self.img1.metrics.mse(self.img5)
+            img1.metrics.mse(img5)
 
     def test_smoothed(self):
         # Compare different images with the same shape / smoothed.
-        self.assertGreater(self.img6.metrics.mse(self.img7), 0)
+        img6 = read(self._test_file('test_smooth.png'))  # smooth
+        img7 = read(self._test_file('test_smoothed.png'))  # smoothed
+        self.assertGreater(img6.metrics.mse(img7), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
