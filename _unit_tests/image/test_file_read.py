145fdb48bf5e180ccf12176768745336d226dc3e
Maxim Remizov <wozimer@gmail.com>
2020-11-30 16:51:10 +0300

    Формируем имя тестового файла из частей и пути к классу

--- _unit_tests/image/test_file_read.py
+++ _unit_tests/image/test_file_read.py
@@ -1,30 +1,44 @@
 import unittest
 
-import cv2
-
 from image import read
 
 
-class ReadImage(unittest.TestCase):
+class TestFileHelper(object):
 
+    def _data_subdir(self):
+        return 'test_data'
+
+    def _test_file(self, *names: str) -> str:
+        import os
+        class_path = os.path.dirname(__file__)
+        data_subdir = self._data_subdir()
+        full_path = os.path.join(class_path, data_subdir, *names)
+        return os.path.normpath(full_path)
+
+
+class ReadImage(unittest.TestCase, TestFileHelper):
     ref_size = 9133995
 
     def test_valid_path(self):
         # Test a valid image path
-        self.assertTrue(read(r'.\test_data\test_read_image.jpg').size == self.ref_size)
+        test_file_name = self._test_file('test_read_image.jpg')
+        self.assertTrue(read(test_file_name).size == self.ref_size)
 
     def test_invalid_path(self):
         # Test an invalid image path
+        test_file_name = self._test_file('foo.jpg')
         with self.assertRaises(FileNotFoundError):
-            read('foo')
+            read(test_file_name)
 
     def test_cyrillic_filename(self):
         # Test cyrillic filename
-        self.assertTrue(read(r'.\test_data\изображение.jpg').size == self.ref_size)
+        test_file_name = self._test_file('изображение.jpg')
+        self.assertTrue(read(test_file_name).size == self.ref_size)
 
     def test_cyrillic_path(self):
         # Test cyrillic filename
-        self.assertTrue(read(r".\test_data\изображения\изображение.jpg").size == self.ref_size)
+        test_file_name = self._test_file('изображения', 'изображение.jpg')
+        self.assertTrue(read(test_file_name).size == self.ref_size)
 
 
 if __name__ == '__main__':
