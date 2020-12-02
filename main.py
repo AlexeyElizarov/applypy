d0a085f1757508d3aa33851bf5fd338e56d3f42c
Alexey Elizarov <haakee@gmail.com>
2020-12-02 17:02:16 +0300

    baseline scenario updated

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-# from magnipy import Detector
-# from magnipy.objects import TelegramNotification
-#
-# import applepy
-#
-from video.base import Video
+from video import Video
+from elements import TelegramNotificationElement
 
-# import PIL
-#
-# import image
-# import video
-#
-# if __name__ == '__main__':
-#     # detector = Detector()
-#     image = image.read(...)
-#     image.write(...)
-#     # object = TelegramNotification()
-#     # out = detector.detect(object, image)
-#
-#     for rect in out:
-#         image.draw(rect)
-#
-#     image.show()
-#
-#
-#     # detector = Detector()
-#     # video = detector.video.read(...)
-#     # # object = TelegramNotification()
-#     # # video = detector.detect(object, image)
-#     # # video.write(...)
+
+def baseline(input_video, output_video):
+    """Baseline scenario is to detect Telegram Notifications in the video file and blur them."""
+
+    element = TelegramNotificationElement()
+
+    with Video(input_video) as video:
+        frames = []
+        elements = video.detect(element)
+        # elements is the list of tuples of frame number and element contour
+
+        for element in elements:
+
+            frame_num, contour = element
+            frame = video.frames.read(frame_num)
+            frame = frame.filter.blur(contour)
+            frames.append((frame_num, frame))
+
+        # frames is the list of tuples of frame number and frame to be replaced in the input video
+        video.write(output_video, frames, codec=video.codec, bitrate=video.bitrate, dimension=video.dimension)
+
+
+if __name__ == '__main__':
+
+    video_in = r".\_unit_tests\video\test_data\test_video_read.mp4"
+    video_out = r".\_unit_tests\video\test_data\test_video_write.mp4"
+
+    baseline(video_in, video_out)
+
