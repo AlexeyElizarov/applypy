efa7aae5b0cd8dd0411b21c4a65bbc0adb479671
Maxim Remizov <wozimer@gmail.com>
2020-12-04 15:48:29 +0300

    Вместо записи изображение сравнивается с шаблоном из файла

--- tests/image/test_mode.py
+++ tests/image/test_mode.py
@@ -1,8 +1,9 @@
 import unittest
-from os.path import join
+
+import numpy
 
 from helpers import TestFileHelper
-from image import read, write
+from image import read
 
 
 class ModeConversions(unittest.TestCase, TestFileHelper):
@@ -10,11 +11,12 @@ class ModeConversions(unittest.TestCase, TestFileHelper):
     def test_to_greyscale(self):
         rgb = read(self._test_file('test_rgb.png'))
         grey = rgb.mode.to_greyscale()
-        with self._temp_dir() as temp_dir:
-            path = join(temp_dir, 'test_greyscale.png')
-            write(path, grey)
         self.assertEqual(rgb.channels, 3)
         self.assertEqual(grey.channels, 0)
+        # ожидаемая картинка размером 100*100 и с одним планом
+        # левая половина заполнена 91, а правая 123
+        expected = numpy.full((100, 100), [91] * 50 + [123] * 50)
+        numpy.testing.assert_array_equal(grey, expected)
 
 
 if __name__ == '__main__':
