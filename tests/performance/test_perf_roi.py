3e6a6f05127936beab00802b381eb1fae1d2e9db
Alexey Elizarov <haakee@gmail.com>
2020-12-08 10:33:14 +0300

    performance test with ROI implementation

---
+++ tests/performance/test_perf_roi.py
@@ -0,0 +1,45 @@
+import time
+import unittest
+
+from elements import TelegramNotificationElement
+from helpers import TestFileHelper
+from video import Video
+
+
+class TestPerformanceFindContours(unittest.TestCase, TestFileHelper):
+
+    def test_performance(self):
+
+        t0 = self.initial()
+        t1 = self.improved()
+
+        print(t0, t1, ((t1 - t0)/t0) * 100)
+
+    def improved(self):
+
+        path = self._test_file('test_perf_roi.mp4')
+        element = TelegramNotificationElement()
+
+        with Video(path) as video:
+            start = time.time()
+            video.detect(element)
+            end = time.time()
+
+        return end - start
+
+    def initial(self):
+
+        path = self._test_file('test_perf_roi.mp4')
+        element = TelegramNotificationElement()
+        element._ROI = None
+
+        with Video(path) as video:
+            start = time.time()
+            video.detect(element)
+            end = time.time()
+
+        return end - start
+
+
+if __name__ == '__main__':
+    unittest.main()
