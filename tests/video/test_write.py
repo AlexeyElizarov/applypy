393ef257c5c9dd7cc370b16d366101c09a27a270
Maxim Remizov <wozimer@gmail.com>
2020-12-09 18:54:52 +0300

    Подправил тест на запись видео

--- tests/video/test_write.py
+++ tests/video/test_write.py
@@ -1,31 +1,34 @@
+import os
 import unittest
 
 from numpy import array
 
-from image import Image
-from video import Video
-
 from helpers import TestFileHelper
+from image import Image
+from video import Video, Writer
 
 
 class TestFrames(unittest.TestCase, TestFileHelper):
 
     def test_write(self):
+        codec = ('a', 'v', 'c', '1')
+        frame_rate = 30
+        width = 320
+        height = 240
 
-        path = self._test_file('test_video_write.mp4')
-        frames = []
-        codec = 'AVC1'
-        framerate = 30
-        dimension = 100
+        with self._temp_dir() as temp_dir:
+            path = os.path.join(temp_dir, 'test_video_write.mp4')
 
-        for color in range(256):
-            img = Image(array([[[color, color, color] for i in range(dimension)] for j in range(dimension)]))
-            frames.append(img)
+            with Writer(path, codec, frame_rate, (width, height)) as writer:
+                for color in range(256):
+                    img = Image(array([[[color, color, color] for x in range(width)] for y in range(height)]))
+                    writer.write([img])
 
-        Video.write(frames, path, codec, framerate, (dimension, dimension))
-
-        with Video(path) as v:
-            self.assertEqual(v.length, 256)
+            with Video(path) as v:
+                self.assertEqual(256, v.length)
+                self.assertEqual(width, v.width)
+                self.assertEqual(height, v.height)
+                self.assertEqual(frame_rate, v.framerate)
 
 
 if __name__ == '__main__':
